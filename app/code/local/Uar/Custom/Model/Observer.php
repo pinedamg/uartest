a646a7d18512a927999c110ad29fa1bc84fe0ac8
MPineda <pinedamg@gmail.com>
2012-08-06 22:31:27 -0300

    fix qty, adding loading mask

--- app/code/local/Uar/Custom/Model/Observer.php
+++ app/code/local/Uar/Custom/Model/Observer.php
@@ -83,6 +83,8 @@ class Uar_Custom_Model_Observer
                 $_product->load($_product->getId());
                 if ($_product->isVirtual() && $_product_item->getId()!=$_product->getId()) {
                     $_cart->removeItem($_item_id);
+                }else if($_product->isVirtual() && $_product_item->getId()==$_product->getId()){
+                    $_cart->updateItem($_item_id,array('qty'=>1));
                 }
             }
         }
